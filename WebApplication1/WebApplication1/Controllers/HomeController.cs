12ad036bb0c69b9468456da8bbd76de5db312429
Amedeo Ronza <amedeoronza@gmail.com>
2024-03-05 17:05:27 +0100

    Aggiungere i file di progetto.

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult InsertCliente()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult InsertCliente(Cliente cliente)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDb"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                conn.Open();
+
+                string query = "INSERT INTO Cliente (idCliente, Nominativo, IsAzienda, codiceFiscale, partitaIva) VALUES ( @idCliente, @Nominativo, @IsAzienda, @codiceFiscale, @partitaIva )";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@idCliente", cliente.idCliente);
+                cmd.Parameters.AddWithValue("@Nominativo", cliente.Nominativo);
+                cmd.Parameters.AddWithValue("@IsAzienda", cliente.IsAzienda);
+                cmd.Parameters.AddWithValue("@codiceFiscale", cliente.codiceFiscale ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@partitaIva", cliente.partitaIva ?? (object)DBNull.Value);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Errore");
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Spedizione.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Spedizione
+    {
+        public int IdSpedizione { get; set; }
+        public int IdCliente { get; set; }
+        [DisplayName("Crea codice tracciamento")]
+        [Required(ErrorMessage = "Il codice è obbligatorio")]
+        [StringLength(50)]
+        public string codTracciamento { get; set; }
+        public DateTime dataSpedizione { get; set; }
+        [DisplayName("Inserisci il peso della spedizione")]
+        [Required(ErrorMessage = "Il codice è obbligatorio")]
+        public decimal pesoSpedizione { get; set; }
+        [DisplayName("Inserisci la citta di destinazione")]
+        [Required(ErrorMessage = "la destinazione e' obbligatoria")]
+        [StringLength(50, MinimumLength = 3,
+        ErrorMessage = "la citta deve essere compresa tra 3 e 50 caratteri")]
+        public string cittaDestinazione { get; set; }
+        [DisplayName("Inserisci l'indirizzo di destinazione")]
+        [Required(ErrorMessage = "l'indirizzo di destinazione e' obbligatorio")]
+        public string indirizzoDestinazione { get; set; }
+        [DisplayName("Inserisci il nominativo del destinatario")]
+        [Required(ErrorMessage = "Il nominativo del destinatario e' obbligatorio")]
+        public string nominativoDestinatario { get; set; }
+        [DisplayName("Inserisci il costo della spedizione")]
+        [Required(ErrorMessage = "Il costo della spedizione e' obbligatorio")]
+        public decimal costoSpedizione { get; set; }
+        public DateTime dataConsegna { get; set; }
+    }
+}
