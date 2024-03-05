ede7b40cb9e86d1b1ef56355fb347a1f9618a100
Amedeo Ronza <amedeoronza@gmail.com>
2024-03-05 22:11:08 +0100

    ParteDueSpedizioni

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -89,5 +89,89 @@ namespace WebApplication1.Controllers
             return View(listaClienti);
 
         }
+        public ActionResult InsertSpedizione()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InsertSpedizione(Spedizione spedizione)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDb"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                conn.Open();
+
+                string query = "INSERT INTO Spedizione (idCliente, codTracciamento, dataSpedizione, pesoSpedizione, cittaDestinazione, indirizzoDestinazione, nominativoDestinatario, costoSpedizione, dataConsegna) VALUES ( @idCliente, @codTracciamento, @dataSpedizione, @pesoSpedizione, @cittaDestinazione, @indirizzoDestinazione, @nominativoDestinatario, @costoSpedizione, @dataConsegna )";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@idCliente", spedizione.idCliente);
+                cmd.Parameters.AddWithValue("@codTracciamento", spedizione.CodTracciamento);
+                cmd.Parameters.AddWithValue("@dataSpedizione", spedizione.DataSpedizione);
+                cmd.Parameters.AddWithValue("@pesoSpedizione", spedizione.PesoSpedizione);
+                cmd.Parameters.AddWithValue("@cittaDestinazione", spedizione.CittaDestinazione);
+                cmd.Parameters.AddWithValue("@indirizzoDestinazione", spedizione.IndirizzoDestinazione);
+                cmd.Parameters.AddWithValue("@nominativoDestinatario", spedizione.NominativoDestinatario);
+                cmd.Parameters.AddWithValue("@costoSpedizione", spedizione.CostoSpedizione);
+                cmd.Parameters.AddWithValue("@dataConsegna", spedizione.DataConsegna);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Errore");
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View();
+        }
+
+        public ActionResult Spedizioni()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDb"].ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+            List<Spedizione> listaSpedizioni = new List<Spedizione>();
+
+            try
+            {
+                conn.Open();
+                string query = "SELECT * FROM Spedizione";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Spedizione spedizione = new Spedizione();
+                    spedizione.IdSpedizione = Convert.ToInt32(reader["IdSpedizione"]);
+                    spedizione.idCliente = Convert.ToInt32(reader["idCliente"]);
+                    spedizione.CodTracciamento = reader["CodTracciamento"].ToString();
+                    spedizione.DataSpedizione = Convert.ToDateTime(reader["DataSpedizione"]);
+                    spedizione.PesoSpedizione = Convert.ToDecimal(reader["PesoSpedizione"]);
+                    spedizione.CittaDestinazione = reader["CittaDestinazione"].ToString();
+                    spedizione.IndirizzoDestinazione = reader["IndirizzoDestinazione"].ToString();
+                    spedizione.NominativoDestinatario = reader["NominativoDestinatario"].ToString();
+                    spedizione.CostoSpedizione = Convert.ToDecimal(reader["CostoSpedizione"]);
+                    spedizione.DataConsegna = Convert.ToDateTime(reader["DataConsegna"]);
+                    listaSpedizioni.Add(spedizione);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View(listaSpedizioni);
+
+        }
     }
 }
--- WebApplication1/WebApplication1/Models/Spedizione.cs
+++ WebApplication1/WebApplication1/Models/Spedizione.cs
@@ -9,30 +9,34 @@ namespace WebApplication1.Models
 {
     public class Spedizione
     {
+        [DisplayName("Id Spedizione")]
         public int IdSpedizione { get; set; }
-        public int IdCliente { get; set; }
-        [DisplayName("Crea codice tracciamento")]
+        [DisplayName("Id Cliente")]
+        public int idCliente { get; set; }
+        [DisplayName("Codice Tracciamento")]
         [Required(ErrorMessage = "Il codice è obbligatorio")]
         [StringLength(50)]
-        public string codTracciamento { get; set; }
-        public DateTime dataSpedizione { get; set; }
-        [DisplayName("Inserisci il peso della spedizione")]
+        public string CodTracciamento { get; set; }
+        [DisplayName("Data Spedizione")]
+        public DateTime DataSpedizione { get; set; }
+        [DisplayName("Peso Spedizione in Kg")]
         [Required(ErrorMessage = "Il codice è obbligatorio")]
-        public decimal pesoSpedizione { get; set; }
-        [DisplayName("Inserisci la citta di destinazione")]
+        public decimal PesoSpedizione { get; set; }
+        [DisplayName("Città")]
         [Required(ErrorMessage = "la destinazione e' obbligatoria")]
         [StringLength(50, MinimumLength = 3,
         ErrorMessage = "la citta deve essere compresa tra 3 e 50 caratteri")]
-        public string cittaDestinazione { get; set; }
-        [DisplayName("Inserisci l'indirizzo di destinazione")]
+        public string CittaDestinazione { get; set; }
+        [DisplayName("Indirizzo")]
         [Required(ErrorMessage = "l'indirizzo di destinazione e' obbligatorio")]
-        public string indirizzoDestinazione { get; set; }
-        [DisplayName("Inserisci il nominativo del destinatario")]
+        public string IndirizzoDestinazione { get; set; }
+        [DisplayName("Nominativo")]
         [Required(ErrorMessage = "Il nominativo del destinatario e' obbligatorio")]
-        public string nominativoDestinatario { get; set; }
-        [DisplayName("Inserisci il costo della spedizione")]
+        public string NominativoDestinatario { get; set; }
+        [DisplayName("Costo in €")]
         [Required(ErrorMessage = "Il costo della spedizione e' obbligatorio")]
-        public decimal costoSpedizione { get; set; }
-        public DateTime dataConsegna { get; set; }
+        public decimal CostoSpedizione { get; set; }
+        [DisplayName("Data Consegna")]
+        public DateTime DataConsegna { get; set; }
     }
 }
